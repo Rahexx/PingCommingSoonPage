2908cbc3bd7517cee0d106e035f18af3aea7e216
Rahexx <Rahexx@users.noreply.github.com>
2025-01-18 10:33:14 +0000

    deploy: 7f8bf77c9ff997bfc0d100d771a077f9317e07d1

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('.form');
 const submit = form.querySelector('.form__submit');
 const emailInput = form.querySelector('.form__input');
+const errorMsg = form.querySelector('.form__error');
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
@@ -8,6 +9,14 @@ submit.addEventListener('click', (e) => {
   e.preventDefault();
   const isEmailValid = emailRegex.test(emailInput.value);
 
-  if (isEmailValid) form.classList.remove('form--error');
-  else form.classList.add('form--error');
+  if (!isEmailValid) {
+    errorMsg.textContent =
+      emailInput.value.length === 0
+        ? 'Whoops! It looks like you forgot to add your email'
+        : 'Please provide a valid email address';
+
+    form.classList.add('form--error');
+  } else {
+    form.classList.remove('form--error');
+  }
 });
